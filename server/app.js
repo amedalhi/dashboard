7e9784530cd3c50db4653ce13a33399617621afb
amedalhi <100897687+amedalhi@users.noreply.github.com>
2024-11-27 21:05:57 -0800

    feat: weather widget

--- server/app.js
+++ server/app.js
@@ -7,8 +7,28 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+app.get("/api/weather", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?lat=36.74&lon=-119.77&units=imperial&appid=57d38405c8bc272d90c7bc1a73396123"
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching weather data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const {
+      name: city,
+      main: { temp },
+    } = data;
+
+    // Send the response
+    res.json({ city, temp: Math.round(temp) });
+  } catch (error) {
+    console.error("Error fetching weather data:", error.message);
+    res.status(500).json({ error: "Failed okay to fetch weather data" });
+  }
 });
 
 app.listen(port, () => {
